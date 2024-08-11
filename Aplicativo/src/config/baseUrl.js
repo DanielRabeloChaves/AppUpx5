64a3a09fa9e86c6e08d03080479f258bdefdc099
DanielRabeloChaves <danielrabelochaves1999@gmail.com>
2024-08-11 18:32:17 -0300

    ajuste rota back app

--- Aplicativo/src/config/baseUrl.js
+++ Aplicativo/src/config/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.100.89:3001/api';
+const baseUrl = `${process.env.EXPO_PUBLIC_API_URL}/api` || 'http://192.168.100.89:3001/api';
 export default baseUrl;
